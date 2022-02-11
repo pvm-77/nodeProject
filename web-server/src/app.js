86293aa817663db28c4905688ed237889fbb39a6
pvm-77 <sfzalig@gmail.com>
2022-02-11 12:10:59 +0530

    new chnages office

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,20 @@
 const path = require('path');
-const express = require('express')
+const hbs=require('hbs');
+const express = require('express');
+const { response } = require('express');
 
 const app = express()
 // set path to public folder for dynamic files
 app.use(express.static(path.join(__dirname, '../public')))
+// set path to views folder for dynamic files
+const partialPath = path.join(__dirname, '../templates/partials')
+const viewsPath=path.join(__dirname, '../templates/views')   
 // set view path
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', viewsPath);
 // set view engine
 app.set('view engine', 'hbs')
+hbs.registerPartials(partialPath);
+
 
 app.get('', function (req, res) {
     res.render('index', { title: 'Home' })
@@ -18,6 +25,11 @@ app.get('/about', function (req, res) {
 
 app.get('/help', function (req, res) {
     res.render('help', { title: 'help' })
+})
+
+app.get('*',(request,response)=>{
+    response.render('404',{title:'404',
+    errorMessage:'Page not found'})
 })
 
 app.listen(3000)
